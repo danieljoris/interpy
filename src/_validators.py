5a21a68474eb91a0e37ea2b0f55665fdfc131aa2
Daniel Joris <d.joris@rioanalytics.com>
2024-07-16 22:27:07 -0300

    Added more custom errors

--- src/_validators.py
+++ src/_validators.py
@@ -1,6 +1,12 @@
 from inspect import Signature
 from typing import Protocol
-from src.errors import InvalidDecoratorUsageError, NotAnInterfaceError
+from src.errors import (
+    InterfaceMethodError,
+    InvalidDecoratorUsageError,
+    MethodNotCallableError,
+    MethodSignatureMismatchError,
+    NotAnInterfaceError,
+)
 
 
 class IsAProtocol:
@@ -20,9 +26,8 @@ class MethodSignatureIsEqual:
         method_name: str,
     ) -> None:
         if not interface_method_signature == class_method_signature:
-            raise TypeError(
-                f"Signature mismatch for method {method_name} in class {class_name}. "
-                f"\nExpected {interface_method_signature}, got {class_method_signature}"
+            raise MethodSignatureMismatchError(
+                method_name, class_name, interface_method_signature, class_method_signature
             )
 
 
@@ -53,13 +58,11 @@ class IsAMethod:
     @classmethod
     def is_satisfied_by(cls, candidate: object, method: str) -> None:
         if not callable(candidate):
-            raise TypeError(f"{method} in class {candidate.__name__} is not callable")
+            raise MethodNotCallableError(method_name=method, class_name=candidate.__name__)
 
 
 class HasTheMethod:
     @classmethod
     def is_satisfied_by(cls, method: str, interface: object):
         if not hasattr(interface, method):
-            raise TypeError(
-                f"Class '{interface.__name__}' does not have method '{method}' declared or implemented."
-            )
+            raise InterfaceMethodError(interface_name=interface.__name__, method_name=method)
--- src/errors.py
+++ src/errors.py
@@ -5,6 +5,9 @@
 # Expected (self, abc: str, de: float) -> bool, got ()
 
 
+from inspect import Signature
+
+
 class NotAnInterfaceError(Exception):
     """Exception raised when a class is not an interface."""
 
@@ -21,5 +24,54 @@ class InvalidDecoratorUsageError(Exception):
 
     def __init__(self, class_object):
         self.class_object_name = class_object.__name__
-        self.message = "The '@implements' decorator can only be used in classes."
+        self.message = self.__generate_message()
         super().__init__(self.message)
+
+    def __generate_message(self) -> str:
+        return "The '@implements' decorator can only be used in classes."
+
+
+class InterfaceMethodError(Exception):
+    """Exception raised when Class does not have specific method declared or implemented."""
+
+    def __init__(self, interface_name: str, method_name: str):
+        self.interface_name = interface_name
+        self.method_name = method_name
+        self.message = self._generate_message()
+        super().__init__(self.message)
+
+    def _generate_message(self) -> str:
+        return f"Class '{self.interface_name}' does not have method '{self.method_name}' declared or implemented."
+
+
+class MethodNotCallableError(Exception):
+    def __init__(self, method_name: str, class_name: str):
+        self.method_name = method_name
+        self.class_name = class_name
+        self.message = self._generate_message()
+        super().__init__(self.message)
+
+    def _generate_message(self) -> str:
+        return f"Method '{self.method_name}' in class '{self.class_name}' is not callable."
+
+
+class MethodSignatureMismatchError(Exception):
+    def __init__(
+        self,
+        method_name: str,
+        class_name: str,
+        expected_signature: Signature,
+        actual_signature: Signature,
+    ):
+        self.method_name = method_name
+        self.class_name = class_name
+        self.expected_signature = expected_signature
+        self.actual_signature = actual_signature
+        self.message = self._generate_message()
+        super().__init__(self.message)
+
+    def _generate_message(self) -> str:
+        return (
+            f"Signature mismatch for method '{self.method_name}' in class '{self.class_name}'. "
+            f"\nExpected: {self.expected_signature}, got: {self.actual_signature}"
+        )
