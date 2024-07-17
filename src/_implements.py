bf7916851634d82c75c8487a6d05ca58ba29ad90
Daniel Joris <d.joris@rioanalytics.com>
2024-07-16 21:21:07 -0300

    Added implements class

---
+++ src/_implements.py
@@ -0,0 +1,58 @@
+import inspect
+from typing import get_type_hints
+
+from src._validators import (
+    IsAProtocol,
+    IsAMethod,
+    IsAClass,
+    HasTheMethod,
+    MethodSignatureIsEqual,
+    MethodTypeHintAreEqual,
+)
+
+
+def implements(*interfaces):
+    __methods_to_ignore = ("__init__", "__subclasshook__")
+
+    def decorator(cls):
+        for interface in interfaces:
+            IsAClass.is_satisfied_by(cls)
+            IsAProtocol.is_satisfied_by(interface)
+
+            interface_methods = inspect.getmembers(interface, predicate=inspect.isfunction)
+
+            for method_name, interface_method in interface_methods:
+                if method_name in __methods_to_ignore:
+                    continue
+
+                HasTheMethod.is_satisfied_by(method_name, cls)
+
+                cls_method = getattr(cls, method_name)
+
+                IsAMethod.is_satisfied_by(cls, method_name)
+
+                # Check method signature
+                interface_method_signature = inspect.signature(interface_method)
+                class_method_signature = inspect.signature(cls_method)
+
+                MethodSignatureIsEqual.is_satisfied_by(
+                    interface_method_signature,
+                    class_method_signature,
+                    cls.__name__,
+                    method_name,
+                )
+
+                # Check method type hints
+                interface_method_type_hints = get_type_hints(interface_method)
+                cls_method_type_hints = get_type_hints(cls_method)
+
+                MethodTypeHintAreEqual.is_satisfied_by(
+                    interface_method_type_hints,
+                    cls_method_type_hints,
+                    class_name=cls.__name__,
+                    method_name=method_name,
+                )
+
+        return cls
+
+    return decorator
